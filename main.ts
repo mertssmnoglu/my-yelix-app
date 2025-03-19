0e81550e002f63c4d86e409e341c40961875c714
Mert Şişmanoğlu <mert190737fb@gmail.com>
2025-03-19 19:04:48 +0000

    fix: Disable dynamic import, use app.loadEndpoints

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
 import { Yelix, requestDataValidationMiddleware } from "jsr:@murat/yelix";
 import * as path from "jsr:@std/path@1.0.8";
 
+const endpoints = [await import('./api/hello.ts')];
+
 async function main() {
   // Port is 3030 by default
   const app = new Yelix();
 
-  // Load endpoints from a 'api' folder
-  const currentDir = Deno.cwd();
-  const API_Folder = path.join(currentDir, 'api');
-  await app.loadEndpointsFromFolder(API_Folder);
+  await app.loadEndpoints(endpoints);
 
   app.docsManager.YelixOpenAPI?.customValidationDescription('min', () => {
     return 'hello world!';
